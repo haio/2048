264baee5bc81d0ef5cf148f4f58f7a7243460d8b
haio <mockingror@gmail.com>
2014-08-02 21:41:39 +0800

    Clean code

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var redis = require('redis');
-var db = redis.createClient(6379, 'localhost');
-//var db = redis.createClient(8359, '10.16.33.62');
-//client.auth('1duvtlet0j:cf090196754d78b7090854a916426092728a1e30');
-
-var routes = require('./routes');
-
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
-
-exports.db = db;
+var router = require('./lib/router');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,9 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-routes(app);
-
-io.on('connection', require('./io'));
+io.on('connection', require('./lib/io'));
+router(app, io);
 
 server.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
